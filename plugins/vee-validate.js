c8c28a0acb13ed3e8a1e9001b988185bfc97cee5
Shai Gerbi <ShaiGerbi@gmail.com>
2020-04-13 17:47:13 +0300

    Add rules to plugin

--- plugins/vee-validate.js
+++ plugins/vee-validate.js
@@ -1,1 +1,7 @@
 import { extend } from 'vee-validate';
+
+import { required } from 'vee-validate/dist/rules';
+import { email } from 'vee-validate/dist/rules';
+
+extend('required', required);
+extend('email', email);
