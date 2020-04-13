d5c2e27df83310ae4d6280eda952b657940992b8
Shai Gerbi <ShaiGerbi@gmail.com>
2020-04-13 17:59:24 +0300

    Add localization for errors

--- plugins/vee-validate.js
+++ plugins/vee-validate.js
@@ -1,7 +1,12 @@
 import { extend } from 'vee-validate';
 
+import { localize } from 'vee-validate';
+import en from 'vee-validate/dist/locale/en.json';
+
 import { required } from 'vee-validate/dist/rules';
 import { email } from 'vee-validate/dist/rules';
 
 extend('required', required);
 extend('email', email);
+
+localize('en', en);
