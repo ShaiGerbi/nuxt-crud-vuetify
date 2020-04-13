c177b15d59427130a898f86c74d2f6102bc600e3
Shai Gerbi <ShaiGerbi@gmail.com>
2020-04-13 18:10:04 +0300

    Add validation-observer component

--- components/UserForm.vue
+++ components/UserForm.vue
@@ -1,60 +1,62 @@
 <template>
     <v-dialog v-model="show" max-width="600px" persistent>
-        <v-card>
+        <validation-observer ref="observer" v-slot="{ handleSubmit, reset }">
+            <v-card>
 
-            <v-card-title>
-                <span class="headline">{{ user.id ? 'Edit User' : 'Create User' }}</span>
-            </v-card-title>
+                <v-card-title>
+                    <span class="headline">{{ user.id ? 'Edit User' : 'Create User' }}</span>
+                </v-card-title>
 
-            <v-card-text>
-                <v-container>
+                <v-card-text>
+                    <v-container>
 
-                    <v-row>
-                        <v-col cols="12" sm="6" md="6">
+                        <v-row>
+                            <v-col cols="12" sm="6" md="6">
                                 <validation-provider v-slot="{ errors }" rules="required" name="name">
                                     <v-text-field v-model="user.name" :error-messages="errors" label="Name*" required></v-text-field>
                                 </validation-provider>
-                        </v-col>
-                        <v-col cols="12" sm="6" md="6">
+                            </v-col>
+                            <v-col cols="12" sm="6" md="6">
                                 <validation-provider v-slot="{ errors }" rules="required" name="username">
                                     <v-text-field v-model="user.username" :error-messages="errors" label="Username*" required></v-text-field>
                                 </validation-provider>
-                        </v-col>
-                    </v-row>
+                            </v-col>
+                        </v-row>
 
-                    <v-row>
-                        <v-col cols="12">
+                        <v-row>
+                            <v-col cols="12">
                                 <validation-provider v-slot="{ errors }" rules="" name="website">
                                     <v-text-field v-model="user.website" :error-messages="errors" label="Website"></v-text-field>
                                 </validation-provider>
-                        </v-col>
-                    </v-row>
+                            </v-col>
+                        </v-row>
 
-                    <v-row>
-                        <v-col cols="12" sm="6" md="6">
+                        <v-row>
+                            <v-col cols="12" sm="6" md="6">
                                 <validation-provider v-slot="{ errors }" rules="required|email" name="email">
                                     <v-text-field v-model="user.email" :error-messages="errors" label="Email*" required></v-text-field>
                                 </validation-provider>
-                        </v-col>
-                        <v-col cols="12" sm="6" md="6">
+                            </v-col>
+                            <v-col cols="12" sm="6" md="6">
                                 <validation-provider v-slot="{ errors }" rules="" name="phone">
                                     <v-text-field v-model="user.phone" :error-messages="errors" label="Phone"></v-text-field>
                                 </validation-provider>
-                        </v-col>
-                    </v-row>
+                            </v-col>
+                        </v-row>
 
-                </v-container>
-                <small>*indicates required field</small>
-            </v-card-text>
+                    </v-container>
+                    <small>*indicates required field</small>
+                </v-card-text>
 
-            <v-card-actions>
-                <v-btn v-if="user.id" @click="deleteUser" color="red" dark text>Delete</v-btn>
-                <v-spacer></v-spacer>
-                <v-btn @click="cancel" text>Cancel</v-btn>
-                <v-btn @click.stop="ok" :loading="loading" color="primary">{{ user.id ? 'Update' : 'Create' }}</v-btn>
-            </v-card-actions>
+                <v-card-actions>
+                    <v-btn v-if="user.id" @click="deleteUser" color="red" dark text>Delete</v-btn>
+                    <v-spacer></v-spacer>
+                    <v-btn @click="reset(cancel)" text>Cancel</v-btn>
+                    <v-btn @click.stop="handleSubmit(ok)" :loading="loading" color="primary">{{ user.id ? 'Update' : 'Create' }}</v-btn>
+                </v-card-actions>
 
-        </v-card>
+            </v-card>
+        </validation-observer>
     </v-dialog>
 </template>
 
