52ca1d0e60b0a9d403cf15e48907832e00da0e2d
Shai Gerbi <ShaiGerbi@gmail.com>
2020-04-17 02:56:37 +0300

    Create settings component

---
+++ components/Settings.vue
@@ -0,0 +1,66 @@
+<template>
+    <v-dialog v-model="show" max-width="290" persistent>
+        <v-card>
+
+            <v-card-title class="headline">
+                Settings
+            </v-card-title>
+
+            <v-card-text>
+                <v-container>
+                    <v-row>
+                        <v-col cols="12">
+                            <v-select :items="languages" item-text="language" item-value="code" v-model="settings.language" label="Languages"></v-select>
+                        </v-col>
+                    </v-row>
+                </v-container>
+            </v-card-text>
+
+            <v-card-actions>
+                <v-spacer></v-spacer>
+                <v-btn @click="cancel" text>Cancel</v-btn>
+                <v-btn @click.stop="ok" color="primary" text>Save</v-btn>
+            </v-card-actions>
+
+        </v-card>
+    </v-dialog>
+</template>
+
+<script>
+export default {
+
+    data() {
+        return {
+
+            settings: {
+                language: this.$i18n.locale,
+            },
+
+            languages: [
+                { language: 'English', code: 'en', },
+                { language: 'Hebrew', code: 'he', },
+            ],
+
+        }
+    },
+
+    props: {
+        show: {
+            type: Boolean,
+            required: false,
+            default: false,
+        },
+    },
+
+    methods: {
+        cancel() {
+            this.$emit('cancel');
+        },
+
+        ok() {
+            this.$emit('ok', this.settings);
+        },
+    },
+
+}
+</script>
--- components/TheHeader.vue
+++ components/TheHeader.vue
@@ -5,20 +5,52 @@
 
         <v-spacer></v-spacer>
 
-        <v-btn icon>
+        <v-btn @click="openSettingsDialog" icon>
             <v-icon>mdi-cog</v-icon>
         </v-btn>
+
+        <settings @ok="saveSettings" @cancel="cancelSettingsDialog" :show="dialogs.settings"></settings>
 
     </v-app-bar>
 </template>
 
 <script>
+import Settings from '@/components/Settings';
+
 export default {
 
     data () {
         return {
             title: 'Users App',
+
+            dialogs: {
+                settings: false,
+            }
         }
+    },
+
+    components: {
+        Settings,
+    },
+
+    methods: {
+        saveSettings(settings) {
+            this.$i18n.setLocale(settings.language)
+            this.closeSettingsDialog();
+        },
+
+        cancelSettingsDialog() {
+            this.closeSettingsDialog();
+        },
+
+
+        openSettingsDialog() {
+            this.dialogs.settings = true;
+        },
+
+        closeSettingsDialog() {
+            this.dialogs.settings = false;
+        },
     },
 
 }
