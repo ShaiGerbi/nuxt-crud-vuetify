e0b705bc3ce678eb963e607ec58c11433a826725
Shai Gerbi <ShaiGerbi@gmail.com>
2020-04-19 14:27:48 +0300

    Add Hebrew language for vee-validate

--- plugins/i18n.js
+++ plugins/i18n.js
@@ -1,6 +1,11 @@
+import { localize } from 'vee-validate';
+
 export default function ({ app }) {
 
     app.i18n.onLanguageSwitched = (oldLocale, newLocale) => {
+
+        // VeeValidate Localization
+        localize(newLocale);
 
     };
 
--- plugins/vee-validate.js
+++ plugins/vee-validate.js
@@ -2,11 +2,38 @@ import { extend } from 'vee-validate';
 
 import { localize } from 'vee-validate';
 import en from 'vee-validate/dist/locale/en.json';
+import he from 'vee-validate/dist/locale/he.json';
 
 import { required } from 'vee-validate/dist/rules';
 import { email } from 'vee-validate/dist/rules';
 
+
 extend('required', required);
 extend('email', email);
 
-localize('en', en);
+
+localize({
+    en,
+    he,
+});
+
+localize({
+    en: {
+        names: {
+            name: 'Name',
+            username: 'Username',
+            website: 'Website',
+            email: 'Email',
+            phone: 'Phone',
+        }
+    },
+    he: {
+        names: {
+            name: 'שם',
+            username: 'שם משתמש',
+            website: 'אתר',
+            email: 'דוא"ל',
+            phone: 'טלפון',
+        }
+    }
+});
