4f734fe07f4c8279f701634191a2b711fb306620
Shai Gerbi <ShaiGerbi@gmail.com>
2021-04-06 21:24:31 +0300

    Add 2 rules to .eslintrc.js
    vue/html-self-closing
    vue/max-attributes-per-line

--- .eslintrc.js
+++ .eslintrc.js
@@ -14,5 +14,8 @@ module.exports = {
   plugins: [
   ],
   // add your custom rules here
-  rules: {}
+  rules: {
+    'vue/html-self-closing': 'off',
+    'vue/max-attributes-per-line': 'off'
+  }
 }
